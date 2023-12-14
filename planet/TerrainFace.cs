583fd99571408d9dc49da725882d86343dc610d0
Mitchell L <mitchell.lowe.21@gmail.com>
2023-12-14 15:56:14 +1100

    Update TerrainFace.cs

--- planet/TerrainFace.cs
+++ planet/TerrainFace.cs
@@ -21,17 +21,43 @@ public class TerrainFace
 	
 	public void ConstructMesh()
 	{
+		// Array of vertices with size determined by resolution
 		Vector3[] vertices = new Vector3[resolution * resolution];
+
+		// Array of 
 		int[] triangles = new int[(resolution-1)*(resolution-1)*6];
-		
+		int triIndex = 0;
+
+		// Loop through columns
 		for (int y = 0; y < resolution; y++)
-		{
+		{	
+			// Loop through rows
 			for (int x = 0; x < resolution; x++)
 			{
+				
+				int i = x + y * resolution;
 				Vector2 percent = new Vector2(x, y)  / (resolution - 1);
 				Vector3 pointOnUnitCube = localUp + (percent.X-0.5f) * 2 * axisA + (percent.Y - -0.5f) * 2 * axisB;
+				vertices[i] = pointOnUnitCube;
+
+				if (x != resolution - 1 && y != resolution - 1)
+				{
+					triangles[triIndex] = i;
+					triangles[triIndex+1] = i+resolution+1;
+					triangles[triIndex+2] = i+resolution;
+					
+					triangles[triIndex+3] = i;
+					triangles[triIndex+4] = i+1;
+					triangles[triIndex+5] = i+resolution+1;
+
+					triIndex += 6;
+				}
 			}
 		}
+		mesh.cl
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.rec
 	}
 	
 }
